c55c9c86cc81183c6097764bc55b450524967b51
chris <git@pythoud.net>
2022-01-16 18:06:15 -0500

    Make StringOrCode class public and add some functions for use in client projects

--- src/org/jcodegen/java/StringOrCode.java
+++ src/org/jcodegen/java/StringOrCode.java
@@ -6,25 +6,42 @@ import java.util.List;
 /**
  * ...
  */
-class StringOrCode<T> {
+public class StringOrCode<T> {
+
     private final String string;
     private final T code;
 
-    StringOrCode(final String string) {
+    public StringOrCode(String string) {
         this.string = string;
         code = null;
     }
 
-    StringOrCode(final T code) {
+    public StringOrCode(T code) {
         this.code = code;
         string = null;
     }
 
-    static <T> List<String> getStrings(final List<StringOrCode<T>> alternatives) {
-        List<String> strings = new ArrayList<String>();
+    public static <T> List<String> getStrings(List<StringOrCode<T>> alternatives) {
+        List<String> strings = new ArrayList<>();
         for (StringOrCode<T> alternative: alternatives)
             strings.add(alternative.toString());
         return strings;
+    }
+
+    public boolean isString() {
+        return code == null;
+    }
+
+    public boolean isCode() {
+        return string == null;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public T getCode() {
+        return code;
     }
 
     @Override
@@ -33,4 +50,5 @@ class StringOrCode<T> {
             return code.toString();
         return string;
     }
+
 }
